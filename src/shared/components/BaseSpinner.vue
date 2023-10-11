87c5c59d0fe48de87b5ddcc3a873ccd76e7ab463
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-10-11 15:52:00 -0400

    feat: add Spinner

---
+++ src/shared/components/BaseSpinner.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup></script>
+
+
+<template>
+    <span class="loader"></span>
+</template>
+
+
+
+<style scoped lang="scss">
+.loader {
+    width: 48px;
+    height: 48px;
+    border: 5px dotted var(--secondary);
+    border-radius: 50%;
+    display: inline-block;
+    position: relative;
+    box-sizing: border-box;
+    animation: rotation 2s linear infinite;
+}
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+</style>
