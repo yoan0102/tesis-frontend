4f1b663c7941a6ee8addb494adc0364746db3702
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-10-11 15:13:19 -0400

    feat: add reating component

---
+++ src/shared/components/BaseRating.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { Icon } from '@iconify/vue';
+defineProps<{
+    rating: number
+}>()
+</script>
+
+
+<template>
+    <Icon v-if="rating === 0" icon="material-symbols:star-rate-outline" class="icon cero" />
+    <Icon v-else-if="rating < 5" icon="material-symbols:star-half-outline" class="icon" />
+    <Icon v-else-if="rating === 5" icon="material-symbols:star-rate" class="icon" />
+</template>
+
+
+
+<style scoped lang="scss">
+.icon {
+    color: var(--primary);
+}
+
+.cero {
+    background-color: transparent;
+}
+</style>
