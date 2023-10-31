635db2402c682847322e6ad35d090fb2f5cc1f4d
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-10-31 16:36:07 -0400

    feat: adding orientaciones

--- src/home/views/Orientaciones.vue
+++ src/home/views/Orientaciones.vue
@@ -19,37 +19,167 @@ import BasePlayListHeader from '@/shared/components/BasePlayListHeader.vue'
             compositor, aunque el público que la recibe también participa de ella, la
             evalúa y la hace suya en dependencia de sus gustos e intereses.
         </p>
-        <h2 class="">Requisitos de la canción infantil</h2>
-        <p class="is-size-5">
-            La canción infantil es la que está dirigida a los niños y las niñas, entre
-            sus géneros se encuentran las nanas, los arrullos, las marchas, los
-            himnos, las rondas infantiles, el romance, los juegos rítmicos y la
-            canción infantil como tal, ella debe poseer requisitos que son
-            indispensables para la realización de un canto correcto, entre ellos
-            podemos mencionar:
-        </p>
-        <ol class="is-size-5">
-            <li>Valor instructivo – educativo.</li>
-            <li>Lenguaje fácil y asequible a los niños y las niñas.</li>
-            <li>
-                Texto comprensible y teniendo en cuenta las características de cada
-                edad.
-            </li>
-            <li>Melodía sencilla.</li>
-            <li>Tonalidad acorde a la extensión de las voces.</li>
-            <li>Letra clara.</li>
-        </ol>
-        <p class="is-size-5">
-            Si tenemos en cuenta estos requisitos, no permitiremos en lo adelante que
-            nuestros niños y nuestras niñas repitan hasta el cansancio canciones de
-            adultos que muchos les aplauden sin conocer las consecuencias que pueda
-            traerles en un futuro.
-        </p>
+        <section id="requariment" class="section">
+            <h2 class="subtitle">Requisitos de la canción infantil</h2>
+            <p class="is-size-5">
+                La canción infantil es la que está dirigida a los niños y las niñas, entre
+                sus géneros se encuentran las nanas, los arrullos, las marchas, los
+                himnos, las rondas infantiles, el romance, los juegos rítmicos y la
+                canción infantil como tal, ella debe poseer requisitos que son
+                indispensables para la realización de un canto correcto, entre ellos
+                podemos mencionar:
+            </p>
+            <ol class="is-size-5">
+                <li>Valor instructivo – educativo.</li>
+                <li>Lenguaje fácil y asequible a los niños y las niñas.</li>
+                <li>
+                    Texto comprensible y teniendo en cuenta las características de cada
+                    edad.
+                </li>
+                <li>Melodía sencilla.</li>
+                <li>Tonalidad acorde a la extensión de las voces.</li>
+                <li>Letra clara.</li>
+            </ol>
+            <p class="is-size-5">
+                Si tenemos en cuenta estos requisitos, no permitiremos en lo adelante que
+                nuestros niños y nuestras niñas repitan hasta el cansancio canciones de
+                adultos que muchos les aplauden sin conocer las consecuencias que pueda
+                traerles en un futuro.
+            </p>
+        </section>
+        <section id="importance" class="mt-4 section">
+            <h2 class="subtitle">Importancia del canto en los niños y las niñas.</h2>
+            <p class="is-size-5">El canto constituye uno de los primeros pasos del niño y la niña hacia la música y su
+                correcto desarrollo
+                depende en gran medida del modelo que se utilice para su enseñanza y el manejo que hagamos de su voz. Este
+                contribuye al desarrollo de las cuerdas vocales, a un correcto uso de la voz al cantar y al hablar, a la
+                postura correcta, a la higiene vocal y física; contribuye al desarrollo de capacidades artístico –
+                musicales, a la interpretación y la sensibilidad artística, al desarrollo de los futuros talentos; tiene un
+                gran valor educativo ya que desarrolla el gusto estético, valores morales, conocimientos, convicciones,
+                formas de pensar, de comportarse, erradica la timidez y los rasgos de sobre valoración, todo esto por tanto
+                hace posible ampliar en ellos la cultura general integral.
+                En esta actividad lo esencial es cantar con buena afinación, voz a agradable, natural y relajada, utilizando
+                un buen fraseo y calidad interpretativa, por medio de él las niñas y los niños reciben la más directa
+                experiencia musical, mientras se sientan, a su vez, productores y gestores. En las distintas edades puede
+                hacerse en colectivo, individual, en pequeños formatos o grandes masas corales, mediante coros, cantorías y
+                otras combinaciones vocales.
+                Reglas para cantar correctamente.
+                Para lograr un correcto canto con los niños hay que tener en cuenta una serie de reglas, que harán posible
+                que los niños entonen, proyecten, articulen y digan correctamente la letra de las canciones.
+            </p>
+            <ul class="is-size-5">
+                <li>No gritar la cantar ni hablar.</li>
+                <li>Cantar con la boca ovalada.</li>
+                <li>Mantener una postura correcta.</li>
+                <li>Realizar los ejercicios de la técnica vocal: (respiración, vocalización y relajación.)</li>
+            </ul>
+        </section>
+        <section id="work" class="mt-4 section">
+            <h2 class="subtitle">¿Cómo trabajar la voz infantil?</h2>
+            <p class="is-size-5">
+                Para trabajar la voz infantil debemos saber que las voces de los niños y las niñas son voces blancas ya que
+                no tiene una clasificación definida en sus voces, por lo general cantan en el registro de mujer, pero es
+                menos expresiva y tiene menos color por lo que se hace necesario tener mucho cuidado para no afectarlos,
+                entre otros podemos mencionar:
+            </p>
+            <ul>
+                <li>No dejarlos cantar cundo tengan alguna afección en el aparato vocal.</li>
+                <li>No forzarle demasiado, obligándolos a producir sonidos muy graves o muy agudos.</li>
+                <li>Escoger obras apropiadas a su edad e intereses, siempre dentro de sus posibilidades vocales.</li>
+                <li>Probar las voces para realizar el montaje con los solistas para la comodidad del canto de la niña o el
+                    niño.
+                </li>
+                <li>No fatigarlos con ensayos largos. Puede dedicársela un cuarto de ora a cada ensayo.</li>
+            </ul>
+            <p class="is-size-5">
+                Es conveniente tener un coro que se prepare sistemáticamente para las actividades político – culturales, con
+                un repertorio adecuado y que se logre con él la calidad requerida para estímulo de los niños, las niñas y el
+                maestro o instructor.
+            </p>
+        </section>
+
+        <section id="montaje" class="section">
+            <h2 class="subtitle">Tipos de montajes de canciones</h2>
+            <p is-size-5>Teniendo en cuenta las voces infantiles se pueden realizar los siguientes tipos de montajes:</p>
+            <ul class="is-size-5">
+                <li>Montaje al unísono: Cuando todos los niños cantan todos a la vez.</li>
+                <li>En forma de canon: Se forman dos o tres grupos y la melodía se va superponiendo hasta lograr una
+                    perfecta</li>
+                armonía.
+                <li>En dos y tres voces: Cuando cantan una misma canción, pero en diferentes voces, pueden ser 1., 2., 3.
+                    voz.</li>
+
+            </ul>
+
+            <article class="section">
+                <h3>Métodos para el montaje de canciones.</h3>
+                <p class="is-size-5">
+                    Para el montaje de canciones existen métodos que debemos conocer, teniendo en cuenta la edad y las
+                    características de los niños, así como la extensión y la complejidad rítmica de las mismas, estos son:
+                </p>
+                <ul class="is-size-5">
+                    <li>Método global: Se utiliza para el montaje de canciones cortas y fáciles de aprender.</li>
+                    <li>Método de frases: Para las canciones que tengan más de dos estrofas.</li>
+                    <li>Método del ritmo en el lenguaje: Para las canciones que presenten un diseño rítmico más complejo.
+                    </li>
+
+                </ul>
+            </article>
+        </section>
+        <section id="meto" class="section">
+            <h2 class="subtitle">Metodología para el aprendizaje y montaje de las canciones infantiles</h2>
+            <p class="is0size-5">
+                Para lograr que el niño o la niña se aprenda de forma rápida y correctamente la canción, tendremos en cuenta
+                los pasos a seguir, ello estará en dependencia de las características de los niños y las niñas, de la
+                canción y el método para el montaje que utilicemos, estos son:
+            </p>
+            <ol>
+                <li>Motivación: Sobre el tema de la canción a tratar. puede hacerse a través de una lámina, una
+                    conversación,</li>
+                etc.
+                <li>Presentación, lectura y análisis del texto siguiendo los tres niveles: Traducción, interpretación y
+                    extrapolación.</li>
+
+                <li>Enseñanza de la letra siguiendo un modelo. Por verso, por estrofa hasta repetirla completa si es
+                    necesario. (Método de fases)</li>
+                <li> Realizar el ritmo en el lenguaje si es necesario. (Método del ritmo en el lenguaje)</li>
+                <li>Escucharla dos o tres veces con atención por un modelo. Puede ser grabada o en vivo.</li>
+                <li>Ejercicios de la técnica vocal: Respiración y vocalización</li>
+                <li>Enseñar la melodía. Por verso, por estrofa, hasta repetirla completa si es necesario. (Método de fases)
+                </li>
+                <li>Marcar pulso, acento, diseño rítmico y poliritmias.</li>
+                <li>Realizar juegos musicales: (eco melódico, el telégrafo, melodía en cadena, etc,)</li>
+
+            </ol>
+        </section>
+        <section class="section" id="vocal">
+            <h2 class="subtitle">
+                Partes que componen el aparato vocal.
+            </h2>
+            <ul>
+                <ul class="is-size-5">
+                    <li>Aparato respiratorio:</li>
+                    _ Vías respiratorias superiores: fosas nasales, faringe y laringe. <br>
+                    _ Vías respiratorias inferiores: tráqueas, bronquios y pulmones.
+                    <li>Diafragma: músculo situado encima del aparato digestivo, separa a este del aparato respiratorio.
+                    </li>
+                    <li>Órganos de la boca: boca, lengua, mandíbula, paladar blando o velo el paladar.</li>
+                    <li>
+                        Resonancia:
+                    </li>
+                    _ Partes óseas: senos maxilares y senos frontales. <br>
+                    _ Partes blandas: Velo del paladar, istmo de la garganta, lengua, mejilla, labios.
+
+                </ul>
+
+            </ul>
+        </section>
     </div>
 </template>
 
 <style scoped lang="scss">
 .content {
     padding: 3rem;
+    padding-bottom: 10rem;
 }
 </style>
