2793acf0f1386f05e5484e63c5f25a646826e266
Yoan Estrada Blanco <yoanestrada01@gmail.com>
2023-11-21 18:11:28 -0500

    fix: routes users

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { routesUsers } from '@/users/routes'
 import { routesAdmin } from '../admin/routes'
 import authGuards from '@/shared/guards/authGuards'
-import UserLayout from '@/users/layouts/UserLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -45,7 +44,6 @@ const router = createRouter({
 
     {
       ...routesUsers,
-      path: '/users',
     },
     // {
     //   ...routesAdmin,
